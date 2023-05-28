465f0b9a5f564264b1203cd7843fce96cb5c7fc7
Collins07 <collinsabaya07@gmail.com>
2023-05-28 14:27:40 +0300

    modify register driver/passenger login

--- js/login.js
+++ js/login.js
@@ -1,19 +1,15 @@
 console.log(45)
 
-// clear input fields on page load
-window.onload = function() {
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
-  }
+// // clear input fields on page load
+// window.onload = function() {
+//     document.getElementById("password").value = "";
+//   }
 
-  // clear input fields on focus
-  document.getElementById("username").addEventListener("focus", function() {
-    this.value = "";
-  });
+//   // clear input fields on focus
 
-  document.getElementById("password").addEventListener("focus", function() {
-    this.value = "";
-  });
+//   document.getElementById("password").addEventListener("focus", function() {
+//     this.value = "";
+//   });
 
 
 
